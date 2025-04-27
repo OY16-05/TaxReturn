14b12e54180c87357435b91746878f944a80aa0a
OY16-05 <yuiokuda0616@gmail.com>
2025-04-27 04:39:57 +0000

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,13 +5,15 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 
+
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
        Paginator::useBootstrap();   //追記
 
-
+       \URL::forceScheme('https');
+  $this->app['request']->server->set('HTTPS','on');
        // Paginator::useBootstrapFive();    公式ドキュメント
        //または Paginator::useBootstrapFour();    公式ドキュメント
     } 
